518b20f01dc2fc6f414603a9637fbdf434ef49f4
EC2 Default User <ec2-user@ip-172-31-18-29.us-east-2.compute.internal>
2020-03-13 09:44:24 +0000

    Add style

--- src/App.vue
+++ src/App.vue
@@ -15,3 +15,9 @@
     }
   }
 </script>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>
--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -20,4 +20,10 @@ export default {
     }
   };
 </script>
+
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
+</style>
   
